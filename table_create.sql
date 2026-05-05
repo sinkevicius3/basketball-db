57233f4b7b5146d089665394e4440d6bf67aeefd
Šarūnas Sinkevičius <sinkevicius.sarunas3@gmail.com>
2026-05-05 21:22:10 +0300

    Add team and player tables

---
+++ table_create.sql
@@ -0,0 +1,26 @@
+create table team (
+  id    int         generated always as identity start with 1 increment by 1,
+  city  varchar(50) not null,
+  name  varchar(50) not null,
+  
+  constraint pk_team primary key (id)
+);
+
+create table player (
+  id          int         generated always as identity start with 1 increment by 1,
+  first_name  varchar(50) not null,
+  last_name   varchar(50) not null,
+  birth_date  date        not null,
+  nationality char(3)     not null,
+  position    char(1),
+  team        int,
+  start_date  date,
+  end_date    date,
+  value       decimal(12, 2),
+
+  constraint pk_player      primary key (id),
+  constraint fk_player_team foreign key (team) references team(id) on update cascade on delete set null,
+  constraint check_pos      check (position in ('G', 'F', 'C')),
+  constraint check_date     check (end_date is null or end_date > start_date),
+  constraint check_val      check (value >= 0)
+);
