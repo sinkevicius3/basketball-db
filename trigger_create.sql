c0567a7870f4c34d0fae20dbec4c64397d0075c6
Šarūnas Sinkevičius <sinkevicius.sarunas3@gmail.com>
2026-05-06 11:50:13 +0300

    Add check_teams_comp trigger

--- trigger_create.sql
+++ trigger_create.sql
@@ -14,3 +14,33 @@ create trigger trg_contract_details
 before delete on team
 for each row
 execute function remove_contract_details();
+
+create or replace function check_teams_comp()
+returns trigger as
+$$
+begin
+  if not exists (
+    select 1 from participates_in
+    where team = new.home_team
+    and competition = new.competition
+  ) then 
+    raise exception 'Home team is not in the competition';
+  end if;
+
+  if not exists (
+    select 1 from participates_in
+    where team = new.away_team
+    and competition = new.competition
+  ) then 
+    raise exception 'Away team is not in the competition';
+  end if;
+
+  return new;
+end;
+$$
+language plpgsql;
+
+create trigger trg_team_comp
+before insert on game
+for each row
+execute function check_teams_comp();
